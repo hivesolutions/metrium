d3fda7273351c6eed2e97d71442f0e09854f7869
joamag <joamag@gmail.com>
2013-06-07 15:52:08 +0100

    initial views for the account

--- src/views/web/account.py
+++ src/views/web/account.py
@@ -43,13 +43,122 @@ from metrium import app
 from metrium import flask
 from metrium import quorum
 
+@app.route("/accounts", methods = ("GET",))
+@quorum.ensure("accounts.list")
+def list_accounts():
+    return flask.render_template(
+        "account/list.html.tpl",
+        link = "accounts",
+        sub_link = "list"
+    )
+
+@app.route("/accounts.json", methods = ("GET",), json = True)
+@quorum.ensure("accounts.list", json = True)
+def list_accounts_json():
+    object = quorum.get_object(alias = True, find = True)
+    accounts = models.Account.find(map = True, sort = [("username", 1)], **object)
+    return accounts
+
+@app.route("/account/new", methods = ("GET",))
+@quorum.ensure("accounts.new")
+def new_account():
+    return flask.render_template(
+        "account/new.html.tpl",
+        link = "accounts",
+        sub_link = "create",
+        account = {
+            "cameras" : {}
+        },
+        errors = {}
+    )
+
+@app.route("/accounts", methods = ("POST",))
+@quorum.ensure("accounts.new")
+def create_account():
+    # creates the new account, using the provided arguments and
+    # then saves it into the data source, all the validations
+    # should be ran upon the save operation
+    account = models.Account.new()
+    try: account.save()
+    except quorum.ValidationError, error:
+        return flask.render_template(
+            "account/new.html.tpl",
+            link = "accounts",
+            sub_link = "create",
+            account = error.model,
+            errors = error.errors
+        )
+
+    # redirects the user to the show page of the set that
+    # was just created (normal workflow)
+    return flask.redirect(
+        flask.url_for("show_account", username = account.username)
+    )
+
 @app.route("/accounts/<username>", methods = ("GET",))
 @quorum.ensure("accounts.show")
 def show_account(username):
     account = models.Account.get(username = username)
     return flask.render_template(
-        "account_show.html.tpl",
+        "account/show.html.tpl",
         link = "accounts",
-        sub_link = "info",
+        sub_link = "show",
         account = account
+    )
+
+@app.route("/account", methods = ("GET",))
+@quorum.ensure("accounts.show_s")
+def show_account_s():
+    username = flask.session["username"]
+    account = models.Account.get(username = username)
+    return flask.render_template(
+        "account/show.html.tpl",
+        link = "accounts",
+        sub_link = "show",
+        account = account
+    )
+
+@app.route("/accounts/<username>/edit", methods = ("GET",))
+@quorum.ensure("accounts.edit")
+def edit_account(username):
+    account = models.Account.get(username = username)
+    return flask.render_template(
+        "account/edit.html.tpl",
+        link = "accounts",
+        sub_link = "edit",
+        account = account,
+        errors = {}
+    )
+
+@app.route("/accounts/<username>/edit", methods = ("POST",))
+@quorum.ensure("accounts.edit")
+def update_account(username):
+    # finds the current account and applies the provided
+    # arguments and then saves it into the data source,
+    # all the validations should be ran upon the save operation
+    account = models.Account.get(username = username)
+    account.apply()
+    try: account.save()
+    except quorum.ValidationError, error:
+        return flask.render_template(
+            "account/edit.html.tpl",
+            link = "accounts",
+            sub_link = "edit",
+            account = error.model,
+            errors = error.errors
+        )
+
+    # redirects the user to the show page of the account that
+    # was just updated
+    return flask.redirect(
+        flask.url_for("show_account", username = username)
+    )
+
+@app.route("/accounts/<username>/delete", methods = ("GET",))
+@quorum.ensure("accounts.delete")
+def delete_account(username):
+    account = models.Account.get(username = username)
+    account.delete()
+    return flask.redirect(
+        flask.url_for("list_accounts")
     )
