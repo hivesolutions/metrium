c795c56b25673e1f1e8e00233a23b05b8344dc3a
joamag <joamag@gmail.com>
2013-06-13 15:24:18 +0100

    better models structure

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -34,14 +34,15 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
+import config
 import account
 import base
-import config
 import conversation
 import log
 import mail
 import pending
 
+from config import *
 from account import *
 from base import *
 from config import *
