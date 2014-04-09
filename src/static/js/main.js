e3d2edaacd7eaa8099dd6c702bb091516efae235
joamag <joamag@gmail.com>
2014-04-09 22:53:57 +0100

    simplified ready structure

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -769,18 +769,7 @@
 })(jQuery);
 
 jQuery(document).ready(function() {
-            // retrieves the reference to the top level
-            // body element to apply the components in it
             var _body = jQuery("body");
-
-            // applies the ui component to the body element (main
-            // element) and then applies the extra component logic
-            // from the composite extensions
-            _body.uapply();
-
-            // registers for the applied event on the body to be
-            // notified of new apply operations and react to them
-            // in the sense of applying the specifics
             _body.bind("applied", function(event, base) {
                         base.uapply();
                     });
