2445a1172d3544c5f9897ccc05f1e4e9a3264ea6
joamag <joamag@gmail.com>
2014-08-04 19:36:45 +0100

    initial support for top employees

--- src/bots/omni_bot.py
+++ src/bots/omni_bot.py
@@ -183,9 +183,9 @@ class OmniBot(base.Bot):
             number_sales = values["number_sales"]
             current_amount = amount_price_vat[-1]
             current_number = number_sales[-1]
-            media = api.info_media_entity(object_id, dimensions = "50x50")
-            url = api.base_url + "omni/media/" + media[0]["secret"] if media else None
-            tuple = (current_amount, current_number, employee, url)
+            media = api.info_media_entity(int(object_id), dimensions = "50x50")
+            image_url = api.base_url + "omni/media/" + media[0]["secret"] if media else None
+            tuple = (current_amount, current_number, employee, image_url)
             top_employees.append(tuple)
 
         top_employees.sort(reverse = True)
