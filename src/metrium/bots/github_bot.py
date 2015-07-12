b57db69cf5d8ed4ee5aeb531ca685121bc1c989e
joamag <joamag@gmail.com>
2015-07-12 22:19:08 +0100

    removed comments

--- src/metrium/bots/github_bot.py
+++ src/metrium/bots/github_bot.py
@@ -58,40 +58,18 @@ class GithubBot(base.Bot):
         api = models.GithubConfig.get_api()
         config = models.GithubConfig.singleton()
 
-        #self.register_callback(api)
         activity = self.activity(api, config)
         commits_total = self.commits_total(api, activity)
-        #top_commiters = self.top_commiters(api)
 
         _github = models.Github.get(raise_e = False)
         if not _github: _github = models.Github()
         _github.commits_total = commits_total
-        #_omni.sales_data = sales_data
-        #_omni.sales_stores = sales_stores
-        #_omni.entries_stores = entries_stores
-        #_omni.top_stores = top_stores
-        #_omni.top_employees = top_employees
         _github.save()
 
         pusher = quorum.get_pusher()
         pusher.trigger("global", "omni.commits_total", {
             "commits_total" : commits_total
         })
-        #pusher.trigger("global", "omni.sales_data", {
-        #    "sales_data" : sales_data
-        #})
-        #pusher.trigger("global", "omni.sales_stores", {
-        #    "sales_stores" : sales_stores
-        #})
-        #pusher.trigger("global", "omni.entries_stores", {
-        #    "entries_stores" : entries_stores
-        #})
-        #pusher.trigger("global", "omni.top_stores", {
-        #    "top_stores" : top_stores
-        #})
-        #pusher.trigger("global", "omni.top_employees", {
-        #    "top_employees" : top_employees
-        #})
 
     def activity(self, api, config):
         activity = dict()
@@ -113,51 +91,3 @@ class GithubBot(base.Bot):
             count[0] += previous_t
             count[1] += current_t
         return count
-
-    def commits_repo(self, api):
-        sales_stores = []
-
-        stats = api.stats_sales(span = 2)
-        for _object_id, values in stats.items():
-            name = values["name"]
-            net_price_vat = values["net_price_vat"]
-            current = net_price_vat[-1]
-            previous = net_price_vat[-2]
-            tuple = (current, previous, name)
-            sales_stores.append(tuple)
-
-        sales_stores.sort(reverse = True)
-        return sales_stores
-
-    def top_repos(self, api):
-        top_stores = []
-
-        stats = api.stats_sales(span = 1)
-        for _object_id, values in stats.items():
-            name = values["name"]
-            number_sales = values["number_sales"]
-            current = number_sales[-1]
-            tuple = (current, name)
-            top_stores.append(tuple)
-
-        top_stores.sort(reverse = True)
-        return top_stores
-
-    def top_commiters(self, api):
-        top_employees = []
-
-        stats = api.stats_employee(unit = "month", span = 1, has_global = True)
-        for object_id, values in stats.items():
-            values = values["-1"]
-            employee = values["employee"]
-            amount_price_vat = values["amount_price_vat"]
-            number_sales = values["number_sales"]
-            current_amount = amount_price_vat[-1]
-            current_number = number_sales[-1]
-            media = api.info_media_entity(int(object_id), dimensions = "64x64")
-            image_url = api.base_url + "omni/media/" + media[0]["secret"] if media else None
-            tuple = (current_amount, current_number, employee, image_url)
-            top_employees.append(tuple)
-
-        top_employees.sort(reverse = True)
-        return top_employees
